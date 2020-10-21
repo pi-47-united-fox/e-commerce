bc3b16d3adbfd3da9d3852fb8f2a824ac98d7d7e
Samuel Aditia <samueladitia95@gmail.com>
2020-10-21 11:55:07 +0700

    Add create order, delete order, checkout, and purchase history

---
+++ server/controllers/cartController.js
@@ -0,0 +1,119 @@
+"use strict";
+
+const { Cart, Product } = require("../models");
+
+class CartController {
+	static getAllProcessed(req, res, next) {
+		Cart.findAll({
+			where: { status: "processing", UserId: req.userData.id },
+			include: { model: Product },
+		})
+			.then((result) => {
+				if (result.length) {
+					result = result.map((el) => {
+						return {
+							id: el.id,
+							UserId: el.UserId,
+							status: el.status,
+							quantity: el.quantity,
+							Product: el.Product,
+						};
+					});
+				}
+				res.status(200).json(result);
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+
+	static addOrder(req, res, next) {
+		const orderData = {
+			UserId: req.userData.id,
+			ProductId: req.body.ProductId,
+			status: "processing",
+			quantity: req.body.quantity,
+		};
+
+		Cart.create(orderData)
+			.then((result) => {
+				if (!result) {
+					next({ name: "BadRequest", message: "Failed to add Order" });
+				} else {
+					const { id, UserId, ProductId, status, quantity } = result;
+					res.status(201).json({ id, UserId, ProductId, status, quantity });
+				}
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+
+	static async finishOrder(req, res, next) {
+		try {
+			let orders = await Cart.findAll({
+				where: { status: "processing", UserId: req.userData.id },
+				include: { model: Product },
+			});
+
+			orders.forEach(async (el) => {
+				try {
+					await Product.update(
+						{ stock: el.Product.stock - el.quantity },
+						{ where: { id: el.Product.id } },
+					);
+				} catch (err) {
+					next(err);
+				}
+			});
+
+			const result = await Cart.update({ status: "finished" }, { where: { status: "processing" } });
+			if (result) {
+				res.status(200).json({ message: "Order finished being processed" });
+			}
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static deleteOrder(req, res, next) {
+		Cart.destroy({ where: { id: req.params.id } })
+			.then((result) => {
+				if (!result) {
+					next({ name: "BadRequest", message: "Failed to Delete Order" });
+				} else {
+					res.status(200).json({ message: "Delete Order successful" });
+				}
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+
+	static getAllFinished(req, res, next) {
+		Cart.findAll({
+			where: { status: "finished", UserId: req.userData.id },
+			include: { model: Product },
+			order: [["createdAt", "ASC"]],
+		})
+			.then((result) => {
+				if (result.length) {
+					result = result.map((el) => {
+						return {
+							id: el.id,
+							UserId: el.UserId,
+							status: el.status,
+							quantity: el.quantity,
+							Product: el.Product,
+						};
+					});
+				}
+				res.status(200).json(result);
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+}
+
+module.exports = CartController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const UsersController = require("../controllers/usersController");
 const { authentication, authorization } = require("../middlewares/security");
 const ProductsController = require("../controllers/productsController");
 const BannersController = require("../controllers/bannersController");
+const CartController = require("../controllers/cartController");
 
 router.get("/", (req, res) => {
 	res.send(`<h1>Hello World</h1>`);
@@ -24,11 +25,11 @@ router.post("/banners", authorization, BannersController.createOne);
 router.patch("/banners/:id", authorization, BannersController.changeActive);
 router.delete("/banners/:id", authorization, BannersController.deleteOne);
 
-router.get("/cart"); // get all still processed order
-router.post("/cart"); // add product to cart
-router.patch("/checkout"); // change all processed cart to finished, lower quantity as well
-router.delete("/cart/:id"); // delete order
+router.get("/cart", CartController.getAllProcessed); // get all still processed order
+router.post("/cart", CartController.addOrder); // add product to cart
+router.patch("/checkout", CartController.finishOrder); // change all processed cart to finished, lower quantity as well
+router.delete("/cart/:id", CartController.deleteOrder); // delete order
 
-router.get("/history"); // get all past finished transaction
+router.get("/history", CartController.getAllFinished); // get all past finished transaction
 
 module.exports = router;
